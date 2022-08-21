ebeafa1e86b8481bf3329e654d2dd4d72afa3cc4
xiaofeng <1735305107@qq.com>
2022-08-21 09:24:53 +0800

    完成了首页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,5 +3,22 @@
 $.ajaxPrefilter(options => {
     //拼接根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    console.log(options.url)
+
+    //统一为权限得接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        }
+    }
+
+    //不论成功与否，都会回调complete函数
+    options.complete = res => {
+        console.log(res)
+        //在此回调函数中，res.responseJSON拿到服务器响应得数据
+        //防止用户手动地址栏输入地址未登录跳转（没token,会报错），进行强制跳转
+        if (res.responseJSON.status === 1 && res.responseJSON.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = './login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+;(function () {
+    getUserInfo()
+})()
+
+//获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //成功得回调函数
+        success: res => {
+            if (res.status != 0) return layui.layer.msg('获取用户信息失败！')
+            renderAvatar(res.data)
+        },
+    })
+}
+
+//渲染用户头像
+function renderAvatar(user) {
+    const name = user.nickname || user.username
+    document.querySelector('.welcome').innerHTML = `欢迎\u00A0\u00A0\u00A0${name}`
+    //根据后台头像的有无来决定用图片头像还是文字头像
+    //图片头像
+    if (user.user_pic) {
+        document.querySelector('.layui-nav-img').src = `${user.user_pic}`
+        document.querySelector('.text-avatar').style.display = 'none'
+    }
+    //文字头像
+    document.querySelector('.layui-nav-img').style.display = 'none'
+    document.querySelector(' .userinfo .layui-nav-img').style.display = 'none'
+
+    //渲染文字
+    document.querySelectorAll('.text-avatar')[0].innerHTML = name[0].toUpperCase()
+    document.querySelectorAll('.text-avatar')[1].innerHTML = name[0].toUpperCase()
+}
+
+//退出操作模块
+document.querySelector('.quite').addEventListener('click', function () {
+    layui.layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+        //清空本地存储
+        localStorage.removeItem('token')
+        //重新跳转到登录页面
+        location.href = './login.html'
+        //关闭提示框
+        layer.close(index)
+    })
+})
